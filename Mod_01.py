0a82d65153cda75fb9d7c3a995d18fc227b8d0b7
danielrivera001 <110069322+danielrivera001@users.noreply.github.com>
2022-08-04 11:05:52 -0700

    Add files via upload

---
+++ Mod_01.py
@@ -0,0 +1,45 @@
+#1.Accept the user's first and last name and print them in reverse order with a space between them
+print("Rivera, Daniel")
+#prints my name with a space
+#2.Accept an integer (n) input from the user and compute the value of n+nn+nnn. For example if n=4 then n + nn + nnn = 4 + 44 + 444 = 492
+#3.Ask the user "What country are you from?" then print the following statement: "I have heard that [input] is a beautiful country!"
+print("What Country are you from?")
+#prints the question
+print("I have heard that Puerto Rico is a beautiful country!")
+#prints the answer
+n = 7
+#designates the number 7 to "n"
+z = n + (n * n) + (n * n * n)
+#define a new variable z that is the summation of the products of n
+print("z =",z)
+#What is the output of the following Python code?
+x = 10
+#designates x as 10
+y = 50
+#designates y as 50
+if (x ** 2 > 100 and y < 100):
+    #if-and statement involving x and y
+   print(x, y)
+#Since x*2 is 20, and 20 is not greater than 10 AND y is 50 and it's less than 100, nothing is printed becuase only 1/2 requirements are satisfied!
+
+#What is the output of the following addition (+) operator, and why does this code chunk execute this way?
+a = [10, 20]
+#creates a list
+b = a
+b += [30, 40]
+print("a =",a)
+print("b =" ,b)
+#becuase a is a list, not a number,b is simply adding to the list, and not adding to each individual number within that list
+#What is the output of the following code and what arithmetic operators is being used here?
+print(2%6)
+#The output prints a 2 and diregards the 6, the percentage symbol acts as a comment within the print function
+#What is the output of the following code and what arithmetic operators are used here?
+print(2 * 3 ** 3 * 4)
+#The output is 216. The double asterisk is an exponential operator whereas a single asterisk is multiplication
+#What is a text editor?
+##Ans: A text editor is the space given where the user can input their code for any given coding language, whether it be python, C-code, or html
+#What is python?
+##Ans: Python is a programming language who's main focus is to establish codes that are easy to read and edit for data analysis and software and wesbite development
+#What is jupyter notebook, what type of python environment is it, and what alternatives are there to jupyter notebook?
+##Ans: Jupyter notebook is a web-based interactibve computer programming platform. It allows users to collaborate on code and run code via web-browser
+
